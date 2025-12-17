98d0f283b43afaf2cc360c04db82a1198522e983
Gregory Croisdale <gtcroisdale1@gmail.com>
2025-12-17 01:21:32 +0100

    fix build

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,13 +2,35 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import wasm from "vite-plugin-wasm";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss(), wasm()],
+	plugins: [
+		react(),
+		tailwindcss(),
+		wasm(),
+		viteStaticCopy({
+			targets: [
+				{
+					src: "../pkg/*.wasm",
+					dest: ".",
+				},
+			],
+		}),
+	],
 	server: {
 		fs: {
 			allow: [".."],
+		},
+	},
+	build: {
+		target: "esnext",
+	},
+	optimizeDeps: {
+		exclude: ["../../pkg"],
+		esbuildOptions: {
+			target: "esnext",
 		},
 	},
 });
